633955b5129760cedb3ff2c50ad5c2207a1b11b8
cadumd <cadu.md2021@gmail.com>
2024-10-05 17:57:43 -0300

    jh

--- execucao.js
+++ execucao.js
@@ -41,18 +41,18 @@ confirmar.addEventListener("click", verificação)
 corrige.addEventListener("click", apagar)
 
 //pegando o servidor pra fazer a contagem HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA
-const servidorRuim = await fetch("./lista.json")
+
+
+const valorVermelho = document.querySelector(".valor-vermelho")
+const valorAmarelo = document.querySelector(".valor-amarelo")
+
+
+setInterval(async() => {
+    const servidorRuim = await fetch("./lista.json")
     const servidorBom = await servidorRuim.json()
+    valorVermelho.textContent = ""
+    valorAmarelo.textContent = ""
 
-// const valorVermelho = document.querySelector(".valor-vermelho")
-// const valorAmarelo = document.querySelector(".valor-amarelo")
-
-
-// setInterval(async() => {
-    
-//     valorVermelho.textContent = ""
-//     valorAmarelo.textContent = ""
-
-//     valorVermelho.textContent += servidorBom["55"]["length"]
-//     valorAmarelo.textContent += servidorBom["40"]["length"]
-// }, 100);
+    valorVermelho.textContent += servidorBom["55"]["length"]
+    valorAmarelo.textContent += servidorBom["40"]["length"]
+}, 100);
